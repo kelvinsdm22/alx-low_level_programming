076edc90727942f462dce38168590e8bbf46386e
KDM <kelvinsdm@gmail.com>
2023-03-17 09:49:21 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,14 +7,22 @@
  */
 int main(void)
 {
-	printf("Size of char: %lu byte(s)\n", sizeof(char));
-	printf("Size of short: %lu byte(s)\n", sizeof(short));
-	printf("Size of int: %lu byte(s)\n", sizeof(int));
-	printf("Size of long: %lu byte(s)\n", sizeof(long));
-	printf("Size of long long: %lu byte(s)\n", sizeof(long long));
-	printf("Size of float: %lu byte(s)\n", sizeof(float));
-	printf("Size of double: %lu byte(s)\n", sizeof(double));
-	printf("Size of long double: %lu byte(s)\n", sizeof(long double));
-	printf("Size of void*: %lu byte(s)\n", sizeof(void*));
+	/**Size for 32 bit**/
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of short: %d byte(s)\n", sizeof(short));
+	printf("Size of int: %d byte(s)\n", sizeof(int));
+	printf("Size of long: %d byte(s)\n", sizeof(long));
+	printf("Size of long long: %d byte(s)\n", sizeof(long long));
+	printf("Size of float: %d byte(s)\n", sizeof(float));
+	printf("Size of double: %d byte(s)\n", sizeof(double));
+	printf("Size of long double: %d byte(s)\n", sizeof(long double));
+
+	/**Size for 64 bit **/
+	printf("Size of char: %ld byte(s)\n", sizeof(char));
+	printf("Size of short: %ld byte(s)\n", sizeof(short));
+	printf("Size of int: %ld byte(s)\n", sizeof(int));
+	printf("Size of long: %ld byte(s)\n", sizeof(long));
+	printf("Size of float: %ld byte(s)\n", sizeof(float));
+	printf("Size of double: %ld byte(s)\n", sizeof(double));
 	return (0);
 }
