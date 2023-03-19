a9e911b4547e50fdf196e62eb6427c4e7d8cc391
KDM <kelvinsdm@gmail.com>
2023-03-19 02:16:31 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,17 +13,24 @@ int main(void)
 	{
 		for (j = i; j < 10; j++)
 		{
-			putchar(i + '0');
-			putchar(',');
-			putchar(' ');
-			putchar(j + '0');
-			if (j < 9)
+			int digits[2] = { i, j};
+			int k;
+
+			for (k = 0; k < 2; k++)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(digits[k] + '0');
+				if (k < 1 || j < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				else
+				{
+					putchar('\n');
+				}
 			}
+			
 		}
 	}
-	putchar('\n');
 	return (0);
 }
